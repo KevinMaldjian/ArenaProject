e8487aebda50ffc50df0e4111a45e667f8fa07c2
Kevin Maldjian <kevinmaldjian@gmail.com>
2018-06-21 00:16:48 -0400

    Added network matchmaking and bullet projectile

---
+++ Assets/Scripts/FireScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FireScript : MonoBehaviour{
+
+    public float timer = 3f;
+    public float speed = 5f; 
+
+
+    void Start()
+    {
+        Invoke("Die", timer);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+
+}
+
+
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public Transform emitZone;
+    public GameObject bullet;
+    public float fireRate = 15f;
+    private float nextTimeToFire = 0f; 
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, emitZone.position, transform.rotation);
+    }
+	
+
+}
